6cdf794877d81f4d4c6b2e4093d8e5cdd3c5ef91
udaydevops-lab <darauday0@gmail.com>
2024-09-06 23:32:47 +0530

    terraform practice

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,22 @@
 output "azs" {
     value = slice(data.aws_availability_zones.azs.names, 0,2)
 }
+
+output "vpc_id" {
+    value = aws_vpc.roboshop.id
+  
+}
+
+output "public_subnet_ids" {
+    value = aws_subnet.public_subnet[*].vpc_id
+  
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private_subnet[*].id
+}
+
+
+output "database_subnet_ids" {
+  value = aws_subnet.database_subnet[*].id
+}
